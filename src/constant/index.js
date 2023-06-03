5efd5cfd9c3e7180421a75cda8b1480f443c56bf
sutton <50450947+Ioome@users.noreply.github.com>
2023-06-03 09:03:43 +0800

    增加常量类

---
+++ src/constant/index.js
@@ -0,0 +1,10 @@
+export class Constants {
+    public static  MAX_VALUE = 100;
+    public static  MIN_VALUE = 0;
+
+    public static  COLORS = {
+        RED: '#FF0000',
+        GREEN: '#00FF00',
+        BLUE: '#0000FF',
+    };
+}
