4828fd60ace23b9eefa83a2f47cc1788c5ff7bef
sutton <50450947+Ioome@users.noreply.github.com>
2023-06-10 11:44:30 +0800

    修复

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     define: {
         'process.env': process.env
     },
-    // envPrefix: 'APP_', 控制前缀
+    // envPrefix: 'APP_', 控制前
     resolve: {
         alias: {
             '@': '/src/',
