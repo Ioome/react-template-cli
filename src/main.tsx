6b431b9781f5e10c011fd3b83482d244ec14dec4
王雪儿 <50450947+Ioome@users.noreply.github.com>
2023-06-18 14:42:52 +0800

    refactor: 删除

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import '@/styles/style.css';
 import { HashRouter } from 'react-router-dom';
-import Loading from '@/pages/loading';
+import Loading from '@/components/loading';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
