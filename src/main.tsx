5ca7d78d8e321c96eee417687c1fad0d0b538b45
王雪儿 <50450947+Ioome@users.noreply.github.com>
2023-06-18 15:23:07 +0800

    feat: 删除不必要

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import '@/styles/style.css';
 import { HashRouter } from 'react-router-dom';
-import Loading from 'components/Loading';
+import Loading from '@/components/Loading/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
